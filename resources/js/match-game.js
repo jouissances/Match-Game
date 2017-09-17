52cc1e575f8a761e40ac44bd0ce4acae6bb51022
Janice Darikho <janicedarikho@gmail.com>
2017-09-17 10:11:59 +0800

    #2 second commit. finished stage 1-32. begin stage 2 with rendering cards.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -5,33 +5,37 @@ var MatchGame = {};
   Renders a 4x4 board of cards.
 */
 
+$(document).ready(function() {
+    MatchGame.renderCards(cardValues, $('#game'))
+});
+
 /*
   Generates and returns an array of matching card values.
  */
 
 MatchGame.generateCardValues = function () {
     var arrayOne = [];
-    var arrayRandom = [];
+    var cardValues = [];
 
     for (var i = 1; i < 9; i++) {
         arrayOne.push(i);
         arrayOne.push(i);
     }
 
-//    console.log(arrayOne);
+    //    console.log(arrayOne);
 
     while (arrayOne.length !== 0) {
         var randomIndex = Math.floor(Math.random() * (arrayOne.length));
         var randomNumber = arrayOne[randomIndex];
-        arrayRandom.push(randomNumber);
+        cardValues.push(randomNumber);
         var arrayOneRemoved = arrayOne.splice(randomIndex, 1);
         arrayOne.push(arrayOneRemoved);
         arrayOne.length--;
-        return true;
+        return cardValues;
     }
 
-//    console.log();
-//    console.log(arrayRandom);
+    //    console.log();
+    //    console.log(cardValues);
 };
 
 /*
@@ -39,15 +43,28 @@ MatchGame.generateCardValues = function () {
   object.
 */
 
-MatchGame.renderCards = function (cardValues, $game) {
+MatchGame.renderCards = function (cardValues, $game {
+        var cardColors = ['hsl(25%, 85%, 65%)', 'hsl(55%, 85%, 65%)', 'hsl(90%, 85%, 65%)', 'hsl(160%, 85%, 65%)', 'hsl(220%, 85%, 65%)', 'hsl(265%, 85%, 65%)', 'hsl(310%, 85%, 65%)', 'hsl(360%, 85%, 65%)'];
+        var game = $('#game');
+        
+        $game.empty();
+        
+        var card = $('cardValues[i]');
+        $('#card').each(cardValues, function(i, cardValues[i]) {
+            '<div class="col-md-3" id="card">' + card '</div>'
+            }); 
+        $('#card').data('card', card);
+        $('#card').data('flipped', false);
+        $('#card').data('color', cardColors[card-1]);
+        
+        $('#game').append(card);
+        };
 
-};
+        /*
+          Flips over a given card and checks to see if two cards are flipped over.
+          Updates styles on flipped cards depending whether they are a match or not.
+         */
 
-/*
-  Flips over a given card and checks to see if two cards are flipped over.
-  Updates styles on flipped cards depending whether they are a match or not.
- */
-
-MatchGame.flipCard = function ($card, $game) {
-
-};
+        MatchGame.flipCard = function ($card, $game) {
+            
+        };
